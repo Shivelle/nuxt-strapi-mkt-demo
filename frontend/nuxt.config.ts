7782f32fad5415c9bb72b8b0b8671bdd5986066b
Shivelle <shivelle@gmx.net>
2022-06-14 17:18:24 +0200

    add base setup for nuxt 3 frontend with strapi default configs

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,12 @@
+import { defineNuxtConfig } from 'nuxt'
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({
+  buildModules: ['@nuxtjs/strapi'],
+  strapi: {
+    url: process.env.STRAPI_URL || 'http://localhost:1337',
+    prefix: '/api',
+    version: 'v4',
+    cookie: {},
+  }
+})
